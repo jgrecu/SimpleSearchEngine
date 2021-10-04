7bf758d8988fb0db7a9509cc4494c673bf8e499b
Jeremy Grecu <jeremy.grecu@sap.com>
2021-10-04 15:06:48 +0100

    JBA - Simple Search Engine - Stage 1/6: String theory

--- task/src/search/Main.java
+++ task/src/search/Main.java
@@ -1,7 +1,23 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String word = scanner.nextLine();
+        findWordIndex(input, word);
+    }
+
+    static void findWordIndex(String input, String word) {
+        String[] words = input.strip().split(" ");
+        int index = -1;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word)) {
+                index = i;
+            }
+        }
+        System.out.println(index > -1 ? index + 1 : "Not found");
     }
 }
