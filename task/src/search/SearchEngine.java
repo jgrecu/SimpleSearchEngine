8f5bd712ef6bbeedab0e22c61d0f983b5ca38eb8
Jeremy Grecu <jeremy.grecu@sap.com>
2021-10-07 16:40:26 +0100

    JBA - Simple Search Engine - Stage 5/6: Inverted Index search

--- task/src/search/SearchEngine.java
+++ task/src/search/SearchEngine.java
@@ -1,26 +1,38 @@
 package search;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SearchEngine {
     private List<String> people;
+    private Map<String, List<Integer>> invertedIndex = new HashMap<>();
 
     public SearchEngine(List<String> people) {
         this.people = people;
+        createInvertedIndex();
+    }
+
+    private void createInvertedIndex() {
+        for (int i = 0; i < people.size(); i++) {
+            for (String word : people.get(i).toLowerCase().strip().split("\\s+")) {
+                if (!invertedIndex.containsKey(word)) {
+                    invertedIndex.put(word, new ArrayList<>(List.of(i)));
+                } else {
+                    invertedIndex.get(word).add(i);
+                }
+            }
+        }
     }
 
     public void searchList(String input) {
-        List<String> temp = new ArrayList<>();
-        for (String entry : people) {
-            if(entry.toLowerCase().contains(input.toLowerCase())) {
-                temp.add(entry);
-            }
-        }
+        List<Integer> temp = invertedIndex.get(input.toLowerCase());
 
-        if (!temp.isEmpty()) {
-            for (String entry : temp) {
-                System.out.println(entry);
+        if (temp != null) {
+            System.out.println(temp.size() + " persons found:");
+            for (int entry : temp) {
+                System.out.println(people.get(entry));
             }
         } else {
             System.out.println("No matching people found.");
